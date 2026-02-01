95840e9f68c928cbd62f90656afc8f0468fc38df
Ildar Bikmamatov <ildar@bayrell.org>
2026-02-01 14:45:22 +0500

    update module version

--- src/BayLang/ModuleDescription.js
+++ src/BayLang/ModuleDescription.js
@@ -31,7 +31,7 @@ BayLang.ModuleDescription = class
 	 * Returns module name
 	 * @return string
 	 */
-	static getModuleVersion(){ return "1.2"; }
+	static getModuleVersion(){ return "1.2.1"; }
 	
 	
 	/**
